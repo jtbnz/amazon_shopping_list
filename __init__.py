c777a310f37bafdf332d488af3981e467dd6bc4d
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-06-30 17:19:32 +1200

    Update __init__.py
    updated setup

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 """Amazon Shopping List integration."""
-from homeassistant.helpers import discovery
+from homeassistant.core import HomeAssistant
+
 DOMAIN = 'amazon_shopping_list'
 
-def setup(hass, config):
+def setup(hass: HomeAssistant, config: dict):
     """Set up the Amazon Shopping List component."""
+    # Import the service from amazon_shopping_list.py
+    from .amazon_shopping_list import setup_service
+    setup_service(hass)
     return True
